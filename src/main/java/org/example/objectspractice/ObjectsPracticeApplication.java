df749d1074fb43cd41bb5df77cc37ea5f60f12ef
전윤호 Uknow <yoon6763@naver.com>
2024-04-14 20:20:13 +0900

    init

---
+++ src/main/java/org/example/objectspractice/ObjectsPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.objectspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectsPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/objectspractice/ObjectsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.objectspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
